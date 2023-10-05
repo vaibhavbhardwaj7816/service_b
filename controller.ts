2dbbaed46ff8d32cab3d7c4879fcaa029375c71e
Vaibhav Bhardwaj <vaibhav.bhardwaj@biz2credit.com>
2023-10-05 11:58:27 +0530

    async/await support upgraded

--- controller.ts
+++ controller.ts
@@ -3,7 +3,7 @@ import { userDetails } from "./userData"
 
 export const fetchUserData = async (request:Request, response:Response) => {
     try{
-        return response.status(200).json({userData:[{userData:userDetails}]})
+        return response.status(200).json([{userData:userDetails}])
     } catch(error:any){
         console.error('Error--->',error.message)
         return response.status(400).json({message: error.message})
--- tests/provider.test.ts
+++ tests/provider.test.ts
@@ -1,8 +1,7 @@
-import {Verifier} from '@pact-foundation/pact'
-import * as path from 'path'
+import { Verifier } from '@pact-foundation/pact'
 
 describe('Pact Verification', () => {
-    it('verifies the provider', () => {
+    it('verifies the provider', async () => {
         const options = {
             provider: 'Service-B',
             providerBaseUrl: 'http://localhost:4321',
@@ -10,22 +9,8 @@ describe('Pact Verification', () => {
             pactBroker: 'http://127.0.0.1:9292',
             pactUrls: [
                 'http://127.0.0.1:9292/pacts/provider/Service-B/consumer/Service-A/latest'
-                //   path.resolve(
-                //     process.cwd(),
-                //     'tests',
-                //     'pacts',
-                //     'web_server-api_server.json'
-                //   ),
             ],
         }
-        return new Verifier(options)
-            .verifyProvider()
-            .then((output: any) => {
-                console.log('Pact Verification Complete!')
-                console.log('Result:', output)
-            })
-            .catch((error: any) => {
-                console.error(error)
-            })
+        return await new Verifier(options).verifyProvider()
     })
 })
